e03fef709e9f5b13b41ae90b9a6ca897405b0b27
ben <brmscheiner@gmail.com>
2016-02-11 16:43:19 -0500

    improved documentation

--- ideogram/converter.py
+++ ideogram/converter.py
@@ -27,7 +27,7 @@ def getSourceFnDef(stack,fdefs,path):
     for x in stack:
         if isinstance(x, ast.FunctionDef):
             for y in fdefs[path]:
-                if ast.dump(x)==ast.dump(y): #probably causing slowdown
+                if ast.dump(x)==ast.dump(y): #probably causing the slowness
                     found = True
                     return y
             raise
