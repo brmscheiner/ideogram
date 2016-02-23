db7f237218eb1d8e789a87f2e2fb97b41b5cc476
ben <brmscheiner@gmail.com>
2016-02-23 17:54:16 -0500

    remove text from pack layout

--- scrapeSource/scrapeSource.py
+++ scrapeSource/scrapeSource.py
@@ -7,10 +7,11 @@ def genGraphData(project_path):
     ASTs        = reader.fetch(project_path)
     fdefs,calls = converter.convert(ASTs,project_path)
     writer.jsonHierarchy(fdefs,calls)
+    writer.jsonGraph(fdefs,calls)
 
 if __name__=="__main__":
     project_path = os.path.join(
                                 'C:\\','Users','scheinerbock','Desktop',
-                                'ideogram','scrapeSource','test','bpl-compyler-master'
+                                'ideogram','scrapeSource','test','self'
                                 )
     genGraphData(project_path)
