362f82b66a12307e0e9d788c80ec0f04f3d0052c
Ben <brmsheiner@gmail.com>
2016-03-22 18:20:44 -0400

    bug report

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup,find_packages
 
+# bug: clean tout, hout, nout.json on each run
+
 setup(name='ideogram',
          version='0.1',
          description="Python developers: Create a visual fingerprint for your project's source code!",
@@ -8,5 +10,5 @@ setup(name='ideogram',
          url='https://github.com/brmscheiner/ideogram',
          packages=['ideogram','ideogram.polarfract'],
          install_requires=['pystache>=0.5.4','requests>=2.9.0'],
-         package_data={'ideogram':['ideogram/templates/*.js','ideogram/templates/*.mustache']}
+         package_data={'ideogram':['templates/*.js','templates/*.mustache']}
          )
